6dcfd341047391ab929d621925319c8b0c95895e
Nithin Peter <nithinpeter7@gmail.com>
2020-04-26 11:13:23 +1000

    Use path.join to make it work cross platform

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
+import * as path from 'path';
+
 export const DEFAULT_TEST_FILE_PATTERNS = [
   '**/*.{test,spec}.{js,jsx,ts,tsx}',
   '**/__tests__/*.{js,jsx,ts,tsx}',
 ];
 
-export const DEFAULT_JEST_PATH = 'node_modules/.bin/jest';
+export const DEFAULT_JEST_PATH =path.join('node_modules','.bin', 'jest');
 
 export const TERMINAL_NAME = 'JestRunIt';
